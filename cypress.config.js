dc919cc76d545e808b8c13ddada255c492496a02
Lstarinskaya <starinskayaliliia@gmail.com>
2024-03-16 21:39:41 +0300

    Added config for 5.2.1

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "ytxnzx",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
