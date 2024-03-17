51c9e5d5233323f7db50ace4c6a38b547ca329b6
Lstarinskaya <starinskayaliliia@gmail.com>
2024-03-18 01:12:24 +0300

    Added baseUrl for 5.2.2

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "ytxnzx",
+
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "https://sqlverifier-live-6e21ca0ed768.herokuapp.com",
+    setupNodeEvents(on, config) {},
   },
 });
--- cypress/e2e/verifier2.cy.js
+++ cypress/e2e/verifier2.cy.js
@@ -1,6 +1,7 @@
 describe("check", () => {
+  const baseUrl = Cypress.config("baseUrl");
   beforeEach(() => {
-    cy.visit("https://sqlverifier-live-6e21ca0ed768.herokuapp.com");
+    cy.visit(baseUrl);
     cy.contains("span", "Account").click();
     cy.contains("span", "Sign in").click();
     cy.get('[data-cy="username"]').click().type("user_student");
@@ -10,19 +11,13 @@ describe("check", () => {
 
   it("verify the presence of Home element and URL", () => {
     cy.contains("span", "Home").should("exist");
-    cy.url().should(
-      "include",
-      "https://sqlverifier-live-6e21ca0ed768.herokuapp.com"
-    );
+    cy.url().should("include", baseUrl);
   });
 
   it("verify the presence of Entities element and URL", () => {
     cy.contains("span", "Entities").should("exist").click();
     cy.contains('a[href="/task"] span', "Task").should("exist").click();
-    cy.url().should(
-      "include",
-      "https://sqlverifier-live-6e21ca0ed768.herokuapp.com/task"
-    );
+    cy.url().should("include", `${baseUrl}/task`);
   });
 
   it("verify the presence of User Tasks element and URL", () => {
@@ -30,10 +25,7 @@ describe("check", () => {
     cy.contains('a[href="/user-task"] span', "User Task")
       .should("exist")
       .click();
-    cy.url().should(
-      "include",
-      "https://sqlverifier-live-6e21ca0ed768.herokuapp.com/user-task"
-    );
+    cy.url().should("include", `${baseUrl}/user-task`);
   });
 
   it("verify the presence of Language element", () => {
@@ -63,10 +55,7 @@ describe("check", () => {
   it("verify the presence of Swagger element and URL", () => {
     cy.contains("span", "Swagger").should("exist").click();
     cy.contains("span", "API").should("exist").click();
-    cy.url().should(
-      "include",
-      "https://sqlverifier-live-6e21ca0ed768.herokuapp.com/docs/docs"
-    );
+    cy.url().should("include", `${baseUrl}/docs/docs`);
   });
 
   it("verify the presence of Account button", () => {
@@ -76,33 +65,18 @@ describe("check", () => {
   it("verify the presence of Settings button and URL", () => {
     cy.contains("span", "Account").should("exist").click(),
       cy.contains("span", "Settings").should("exist").click(),
-      cy
-        .url()
-        .should(
-          "include",
-          "https://sqlverifier-live-6e21ca0ed768.herokuapp.com/account/settings"
-        );
+      cy.url().should("include", `${baseUrl}/account/settings`);
   });
 
   it("verify the presence of Password button and URL", () => {
     cy.contains("span", "Account").should("exist").click();
     cy.contains("span", "Password").should("exist").click(),
-      cy
-        .url()
-        .should(
-          "include",
-          "https://sqlverifier-live-6e21ca0ed768.herokuapp.com/account/password"
-        );
+      cy.url().should("include", `${baseUrl}/account/password`);
   });
 
   it("verify the presence of Sign out button and URL", () => {
     cy.contains("span", "Account").should("exist").click();
     cy.contains("span", "Sign out").should("exist").click(),
-      cy
-        .url()
-        .should(
-          "include",
-          "https://sqlverifier-live-6e21ca0ed768.herokuapp.com/logout"
-        );
+      cy.url().should("include", `${baseUrl}/logout`);
   });
 });
