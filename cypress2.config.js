cb640f90b8b4823d75c43776c2e3bca2cfec04c8
Lstarinskaya <starinskayaliliia@gmail.com>
2024-03-18 02:05:43 +0300

    Added second config-file for 5.2.3

---
+++ cypress2.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: "ytxnzx",
+
+  e2e: {
+    baseUrl: "https://sqlverifier-staging-08050d656f7a.herokuapp.com",
+    setupNodeEvents(on, config) {},
+  },
+});
